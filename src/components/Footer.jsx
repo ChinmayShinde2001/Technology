9c9017fdb9176e18d659f12af99c9d110cd76e77
ChinmayShinde2001 <chinmayshinde771@gmail.coom>
2023-07-29 23:28:08 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return <footer>
+    <div>
+        <h1>Technos</h1>
+        <p>@all right reserverd</p>
+    </div>
+    <div>
+        <h5>Follow Us</h5>
+        <div>
+            <a href="https://YouTube.com" target={"blank"}>YouTube</a>
+            <a href="https://instagram.com/chinmay.shinde?igshid=MzRlODBiNWFlZA==" target={"blank"}>Instgram</a>
+            <a href="https://github.com/ChinmayShinde2001" target={"blank"}>GitHub</a>
+         
+
+        </div>
+    </div>
+  </footer>
+}
+
+export default Footer
